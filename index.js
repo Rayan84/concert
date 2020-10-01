a5a6ea4eb241537a6a61d8cff9e31081d7dda128
Rayan84 <65650520+Rayan84@users.noreply.github.com>
2020-10-01 10:35:13 +0200

    Add links to mobile nav Add JavaScript for mobile nav

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-
-
 function appear() {
   var homepageFooter = document.getElementById('homepage-footer');
   var scrollingElement = (document.scrollingElement || document.body);
   homepageFooter.style.display = 'block';
   scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
+
+function mobileNav() {
+  var mobilNavUl = document.querySelector('.mobile-nav-ul');
+  if (mobilNavUl.style.display == 'none') {
+    console.log('1');
+    mobilNavUl.style.display = 'block';
+  }else {
+    console.log('2');
+    mobilNavUl.style.display = 'none';
+  }
 }
